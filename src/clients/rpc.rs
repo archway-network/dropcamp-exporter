8b9af97b77bd48855d322676f5fdb6a5735b0823
Augusto Elesbão <aelesbao@users.noreply.github.com>
2024-04-02 20:09:41 +0200

    feat: rate limit rpc calls

--- src/clients/rpc.rs
+++ src/clients/rpc.rs
@@ -1,7 +1,20 @@
+use std::sync::Arc;
+use std::task::Poll;
+use std::time::Duration;
+
 use anyhow::{anyhow, Result};
 use cosmos_sdk_proto::prost::Message;
+use futures::prelude::future::{BoxFuture, FutureExt};
 use tendermint::block::Height;
-use tendermint_rpc::{client::CompatMode, Client, HttpClient, Url};
+use tendermint_rpc::endpoint::abci_query;
+use tendermint_rpc::{
+    client::CompatMode, Client, Error as TmRpcError, HttpClient, Url as TmRpcUrl,
+};
+use tokio::sync::Mutex;
+use tower::limit::RateLimitLayer;
+use tower::util::BoxService;
+use tower::{BoxError, Service, ServiceBuilder, ServiceExt};
+use url::Url;
 
 use crate::block::Block;
 
@@ -9,7 +22,7 @@ use crate::block::Block;
 /// encoded message for a single block height.
 #[derive(Debug)]
 pub struct RpcClient {
-    inner: HttpClient,
+    svc: Mutex<BoxService<abci_query::Request, abci_query::Response, BoxError>>,
     block: Block,
 }
 
@@ -23,7 +36,7 @@ impl RpcClient {
     }
 
     #[tracing::instrument(fields(height = self.block.height.value()), skip(self, data))]
-    pub async fn request<T, R>(&self, service: &str, method: &str, data: T) -> anyhow::Result<R>
+    pub async fn request<T, R>(&self, service: &str, method: &str, data: T) -> Result<R>
     where
         T: Message + Default,
         R: Message + Default,
@@ -31,17 +44,18 @@ impl RpcClient {
         tracing::debug!(?data, "request");
 
         let path = format!("/{service}/{method}");
-        let response = self
-            .inner
-            .abci_query(
-                Some(path),
-                data.encode_to_vec(),
-                Some(self.block.height),
-                false,
-            )
-            .await?;
+        let request = abci_query::Request::new(
+            Some(path),
+            data.encode_to_vec(),
+            Some(self.block.height),
+            false,
+        );
 
-        let response: R = R::decode(&*response.value)?;
+        let mut svc = self.svc.lock().await;
+        let client = svc.ready().await.map_err(|err| anyhow!(err))?;
+        let response = client.call(request).await.map_err(|err| anyhow!(err))?;
+
+        let response: R = R::decode(&*response.response.value)?;
         tracing::debug!(?response, "response");
 
         Ok(response)
@@ -74,7 +88,16 @@ impl Builder {
         let block = self.get_block(&client).await?;
         tracing::info!(block = ?block, "creating rpc client for block");
 
-        Ok(RpcClient { inner, block })
+        let svc = ServiceBuilder::new()
+            .option_layer(
+                self.rate_limit
+                    .map(|num| RateLimitLayer::new(num, Duration::from_secs(1))),
+            )
+            .service(HttpClientWrapper::new(client))
+            .boxed()
+            .into();
+
+        Ok(RpcClient { svc, block })
     }
 
     async fn get_block(&self, client: &HttpClient) -> Result<Block> {
@@ -91,5 +114,33 @@ impl Builder {
         };
 
         Ok(block.block.header.try_into()?)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct HttpClientWrapper {
+    client: Arc<HttpClient>,
+}
+
+impl HttpClientWrapper {
+    pub fn new(client: HttpClient) -> Self {
+        Self {
+            client: Arc::new(client),
+        }
+    }
+}
+
+impl Service<abci_query::Request> for HttpClientWrapper {
+    type Response = abci_query::Response;
+    type Error = TmRpcError;
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    fn call(&mut self, req: abci_query::Request) -> Self::Future {
+        let client = self.client.clone();
+        async move { client.perform(req).await }.boxed()
+    }
+
+    fn poll_ready(&mut self, _cx: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
     }
 }
